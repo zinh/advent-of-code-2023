550abcbed93e4a8bc6466b3aef350593fb2e1fcb
Vinh <zinh@users.noreply.github.com>
2023-12-11 15:48:40 +0900

    part 2

--- day05/app/Part2.hs
+++ day05/app/Part2.hs
@@ -12,7 +12,11 @@ main = do
 process :: String -> Int
 process content =
   let lstLine = lines content
-      seeds = getSeeds (head lstLine)
+      seedRanges = (split 2) . getSeeds . head $ lstLine
+      seeds = [i | r <- seedRanges, i <- [(head r) .. ((head r) + (r !! 1) - 1)]]
       m = getMapLst . drop 2 $ lstLine
    in minimum . map (processSeed m) $ seeds
 
+split :: Int -> [a] -> [[a]]
+split n [] = []
+split n lst = take n lst : split n (drop n lst)
